2ed436be1528ddea9ed9b05925223c394a860897
Ella Väyrynen <ellajalkkis97@gmail.com>
2018-10-08 14:08:37 +0300

    Added constructor to receive first and lastname and print out

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,32 @@
 package emailapp;
 
-public class Email {
 
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	//Constructor to receive the first name and last name
+	public Email(String firstName, String lastName) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	System.out.println("EMAIL CREATED "+ this.firstName + " " + this.lastName);
+		
+	}
+	
+	//Ask for department
+	
+	//Generate password
+	
+	//Set mailbox capacity
+	
+	//Set an alternate email
+	
+	//Change the password
+	
+	
 }
